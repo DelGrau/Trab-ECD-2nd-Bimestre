6e8257c07e93b192f91e8f3b3a83dc44e229cd39
Alan Henrique <alanhipermassa@gmail.com>
2023-06-05 17:45:06 -0300

    Inicio do projeto

---
+++ TrabalhoECD/src/trabalhoecd/display/Main.java
@@ -0,0 +1,17 @@
+package trabalhoecd.display;
+
+/**
+ *
+ * @author Alan H
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        
+        menu.mainMenu();
+    }
+}
